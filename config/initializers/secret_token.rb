72d3352f7b81636572a39fe5e0e029ad09a12e79
brogupta <bgupta91@gmail.com>
2013-02-02 03:16:42 -0500

    Creating git repository for bizhub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bizhub::Application.config.secret_token = '542713c842c7c8c36a389811acc59b986988d25c080b9fcd56edeb4fcad5e81c2b459ebfb10d8dbf487d69f7e6d382bb7e0db8b3a78129d7e6efc9ae55be49d5'
